fdc0acfd985575fcdfcdbfd12532ab67b5bb6f26
dimhold <dimhold@gmail.com>
2014-07-02 11:58:36 +0300

    Add randos to rando view

--- front-end/js/controller/randosController.js
+++ front-end/js/controller/randosController.js
@@ -3,6 +3,17 @@ var randosController = angular.module("randosController", ['ngResource']);
 randosController.controller("RandosController", function($scope, $http, $routeParams) {
     var token = "abcd";
     $http.get('/admin/randos?token=' + token).success(function (randos) {
+        for (var i = 0; i < randos.length; i++) {
+            randos[i].creationPritty = moment.unix(randos[i].creation).format('DD MMMM YYYY, HH:mm:ss');
+        }
         $scope.randos = randos;
+        $scope.flipRando = function (rando, $event) {
+            var img = $event.target;
+            if (img.src.indexOf("img.s") != -1) {
+                img.src = rando.mapSizeURL.small;
+            } else {
+                img.src = rando.imageSizeURL.small;
+            }
+        };
     });
 });
