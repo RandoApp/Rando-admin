b13610ac596329cad9b90b3910529ab67797d1bc
Viktar Patotski <xp.vit.blr@gmail.com>
2016-09-13 18:03:52 +0300

    remove auth

--- src/service/access.js
+++ src/service/access.js
@@ -1,26 +1,20 @@
 var adminModel = require("../model/adminModel");
+var basicAuth = require('basic-auth');
 
 module.exports = {
     forAdmin: function (req, res, next) {
-        var token = req.headers.authorization;
-        if (!token) {
-            console.info("[admin.forAdmin] token not exist. Send auth.html");
-            res.status(401);
-            res.sendfile("front-end/auth.html");
-            return;
-        }
 
-        adminModel.getByToken(token, function (err, admin) {
+/*        adminModel.getByToken("token", function (err, admin) {
             if (!admin || admin.expiration < Date.now() || token != admin.authToken) {
                 console.log("Send auth.html");
                 res.status(401);
                 res.sendfile("front-end/auth.html");
                 return;
-            }
-
-            if (admin) {
+            }*/
+            var user = basicAuth(req);
+            if (basicAuth) {
                 next();
             }
-        });
+       // });
     }
 };
